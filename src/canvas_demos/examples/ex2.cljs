40d341a63e5556df7af7bfce72e4df0011647b5f
Thomas Getgood <thomas.getgood@gmail.com>
2017-08-28 18:13:11 -0400

    Animations with new shapes.
    Again trivial.
    
    This is great sign. So far solving problems for static case solves the
    animation case for us. Maybe this should have been obvious, but as it is
    I'm pretty happy about it.

--- src/canvas_demos/examples/ex2.cljs
+++ src/canvas_demos/examples/ex2.cljs
@@ -1,6 +1,7 @@
 (ns canvas-demos.examples.ex2
   "Basic Animation Demo"
-  (:require [canvas-demos.drawing :as drawing]))
+  (:require [canvas-demos.drawing :as drawing]
+            [canvas-demos.shapes.base :as base :refer [circle]]))
 
 (defn rc []
   (str "rgb("
@@ -10,11 +11,10 @@
 (defn cfn [x y r]
   (lazy-seq
    (cons
-    [{:type :circle
-      :c [x y]
-      :r r
-      :style {:stroke-style (rc)
-              :line-width 10}}]
+    (circle {:c [x y]
+             :r r
+             :style {:stroke-style (rc)
+                     :line-width 10}})
     (cfn (+ x (- 15 (rand-int 30)))
          (+ y (- 15 (rand-int 30)))
          (max 0 (+ r (- 5 (rand-int 10))))))))
@@ -22,6 +22,9 @@
 (defn c-seq []
   (cfn (rand-int 1000) (rand-int 1000) (rand-int 100)))
 
+(defn combine [& shapes]
+  (into (base/drawing) shapes))
+
 (defn go-circles! []
   (.log js/console "Run (stop!) to kill animation.")
-  (drawing/animate! (apply map concat (take 100 (repeatedly c-seq)))))
+  (drawing/animate! (apply map combine (take 100 (repeatedly c-seq)))))
