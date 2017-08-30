44067b6ba7a3068228da613b0a33183aa383f904
Thomas Getgood <thomas.getgood@gmail.com>
2017-08-30 18:01:35 -0400

    Zoom and pan in working correctly.

--- src/canvas_demos/events.cljs
+++ src/canvas_demos/events.cljs
@@ -10,7 +10,8 @@
   (string/replace (name kw) #"-" ""))
 
 (defn c-space-point [e]
-  [(js/parseInt (.-x e)) (js/parseInt (.-y e))])
+  (let [[_ h] (canvas/canvas-container-dimensions)]
+    [(js/parseInt (.-x e)) (- h (js/parseInt (.-y e)))]))
 
 (defn normalise-zoom [dz]
   (let [base js/Math.E
@@ -20,46 +21,37 @@
 (defn c-space->r-space
   [[x y]]
   (let [{z :zoom [ox oy] :offset} @window]
-    [(- (/ x z) ox) (- (/ y z) oy)]))
+    [(/ (- x ox) z) (/ (- y oy) z)]))
 
-(defn r-norm
-  [[x y]]
-  (let [[_ h] (canvas/canvas-container-dimensions)
-        {z :zoom [ox oy] :offset} @window]
-    [(- (/ x z) ox) (- (/ (- h y) z) oy)]))
+(defn zoom-c [dz ox zx]
+  (+ (* dz ox) (* zx (- 1 dz))))
 
-(defn homothetic
-  "Applies homothetic tx centred at p with scale z to point q"
-  [p q z dz]
-  (let [f (fn [z dz px qx] (+ (* z qx) (* (- 1 z) px)))]
-    (mapv (partial f z dz) p q)))
-
-(defn update-zoom [{z :zoom  q :offset} p dz]
-  {:zoom (* z dz)
-   :offset (homothetic p q dz)})
+(defn update-zoom [{z :zoom o :offset} p dz]
+  (let [zc p]
+    {:zoom (* z dz)
+     :offset (mapv (partial zoom-c z dz) o zc)}))
 
 (defn update-offset [{:keys [zoom] :as w} [dx dy]]
   (update w :offset
           (fn [[x y]]
-            [(- x dx) (+ y dy)])))
+            [(- x dx) (- y dy)])))
 
 (def handlers
   {:mouse-down (fn [e]
                  (reset! drag-state (c-space-point e)))
    :click (fn [e]
-            (.log js/console (r-norm (c-space-point e))))
+            (.log js/console (c-space->r-space (c-space-point e))))
    :mouse-up   (fn [e]
                  (reset! drag-state nil))
    :mouse-move (fn [e]
                  (when @drag-state
                    (let [q     (c-space-point e)
                          p     @drag-state
-                         delta (apply mapv -
-                                      (map c-space->r-space [p q]))]
+                         delta (mapv - p q)]
                      (reset! drag-state q)
                      (swap! window update-offset delta))))
    :wheel      (fn [e]
-                 (let [p  (c-space->r-space (c-space-point e))
+                 (let [p  (c-space-point e)
                        dz (normalise-zoom (js/parseInt (.-deltaY e)))]
                    (swap! window update-zoom p dz)))})
 
