d662b24bde16ad59feda7b52452b281b92f70391
Thomas Getgood <thomas.getgood@gmail.com>
2017-08-31 11:17:05 -0400

    Cleanup and some comments.

--- src/canvas_demos/events.cljs
+++ src/canvas_demos/events.cljs
@@ -6,10 +6,19 @@
 
 (def ^:private drag-state (atom nil))
 
-(defn kw->js [kw]
-  (string/replace (name kw) #"-" ""))
+;;;;; Pan and Zoom logic
 
-(defn c-space-point [e]
+(defn c-space->r-space
+  "Inverse of Projectable/project for vectors. Currently only used for
+  debugging."
+  [[x y]]
+  (let [{z :zoom [ox oy] :offset} @window]
+    [(/ (- x ox) z) (/ (- y oy) z)]))
+
+(defn c-space-point
+  "Returns Inverted pixel coordinates (origin in the lower left) of the event
+  e."
+  [e]
   (let [[_ h] (canvas/canvas-container-dimensions)]
     [(js/parseInt (.-x e)) (- h (js/parseInt (.-y e)))]))
 
@@ -18,29 +27,24 @@
         scale 100]
     (js/Math.pow base (/ (- dz) scale))))
 
-(defn c-space->r-space
-  [[x y]]
-  (let [{z :zoom [ox oy] :offset} @window]
-    [(/ (- x ox) z) (/ (- y oy) z)]))
-
 (defn zoom-c [dz ox zx]
   (+ (* dz ox) (* zx (- 1 dz))))
 
 (defn update-zoom [{z :zoom o :offset} p dz]
   (let [zc p]
     {:zoom (* z dz)
-     :offset (mapv (partial zoom-c z dz) o zc)}))
+     :offset (mapv (partial zoom-c dz) o zc)}))
 
 (defn update-offset [{:keys [zoom] :as w} [dx dy]]
   (update w :offset
           (fn [[x y]]
             [(- x dx) (- y dy)])))
 
+;;;;; Handlers
+
 (def handlers
   {:mouse-down (fn [e]
                  (reset! drag-state (c-space-point e)))
-   :click (fn [e]
-            (.log js/console (c-space->r-space (c-space-point e))))
    :mouse-up   (fn [e]
                  (reset! drag-state nil))
    :mouse-move (fn [e]
@@ -55,6 +59,11 @@
                        dz (normalise-zoom (js/parseInt (.-deltaY e)))]
                    (swap! window update-zoom p dz)))})
 
+
+;;;;; Handler Registration
+
+(defn kw->js [kw]
+  (string/replace (name kw) #"-" ""))
 
 (defonce registered-listeners (atom nil))
 
