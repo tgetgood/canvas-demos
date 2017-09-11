02a82893eb6936b1147220536596e6bf73829e64
Thomas Getgood <thomas.getgood@gmail.com>
2017-09-11 14:40:50 -0400

    Implemented translate and rotate.
    Shockingly straight forward. I'm using canvas' mutable global affine
    transform as a stack, and it seems to simplify everything very nicely.

--- src/canvas_demos/examples/ex3.cljs
+++ src/canvas_demos/examples/ex3.cljs
@@ -1,6 +1,7 @@
 (ns canvas-demos.examples.ex3
   "Demo of shape manipulation via affine transformations"
   (:require [canvas-demos.drawing :as drawing]
+            [canvas-demos.shapes.affine :as affine]
             [canvas-demos.shapes.base :refer [line rectangle]]))
 
 (def boat
@@ -13,5 +14,12 @@
    (line {:p [490 540] :q [420 440]})
    (line {:p [510 540] :q [580 440]})])
 
+(def picture
+  [(-> boat
+       (affine/rotate [500 440] 45))
+   (-> boat
+       #_(affine/translate 400 400))])
+
+
 (defn draw! []
-  (drawing/draw! boat))
+  (drawing/draw! picture))
--- src/canvas_demos/shapes/affine.cljs
+++ src/canvas_demos/shapes/affine.cljs
@@ -1,10 +1,47 @@
 (ns canvas-demos.shapes.affine
-  (:require [canvas-demos.drawing :as drawing]))
+  "Declarative DSL for applying affine transforms to shapes. Isolates
+  transformation to the shape to which it is applied. Can be nested.
+  N.B.: Affine transformations don't commute, so the order in which you apply
+  the basic transformers is important."
+  (:require [canvas-demos.canvas :as canvas]
+            [canvas-demos.drawing :as drawing]))
 
-(defrecord AffineWrapper [atx base-shape]
+(defn det [a b c d]
+  (- (* a d) (* b c)))
+
+(defn invert-atx
+  "Returns matrix corresponding to the inverse affine transform."
+  [[a b c d x y]]
+  (let [abs (det a b c d)
+        [a' b' c' d'] (map (partial * abs) [d (- b) (- c) a])
+        x' (- (+ (* a' x) (* c' y)))
+        y' (- (+ (* b' x) (* d' y)))]
+    [a' b' c' d' x' y']))
+
+(defrecord AffineWrapper [base-shape atx]
+  ;; Draw this shape with the given affine transform. Reset the global state
+  ;; after so as to not effect other shapes.
+  ;; N.B.: requires serial rendering.
   drawing/Drawable
   (draw [_ ctx]
-    (let [before-atx (.-currentTransform ctx)]
-      (.transform ctx atx)
-      (draw base-shape ctx)
-      (.setTransform ctx before-atx))))
+    (canvas/apply-affine-tx ctx atx)
+    (drawing/draw base-shape ctx)
+    (canvas/apply-affine-tx ctx (invert-atx atx))))
+
+(defn deg->rad [d]
+  (* js/Math.PI (/ d 180)))
+
+(defn translate [shape x y]
+  (AffineWrapper. shape [1 0 0 1 x y]))
+
+(defn rotate
+  "Returns a copy of shape rotated by angle around the given centre of
+  rotation. units can be either :degrees (default) or :radians."
+  [shape [x y :as centre] angle & [units]]
+  (let [r (if (= units :radians) angle (deg->rad angle))
+        c (Math.cos r)
+        s (Math.sin r)]
+    (-> shape
+        (translate (- x) (- y))
+        (AffineWrapper. [c s (- s) c 0 0])
+        (translate x y))))
