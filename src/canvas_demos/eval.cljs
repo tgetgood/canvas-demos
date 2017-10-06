ed34cbd4105c528bb92508a533f1225008186aca
Thomas Getgood <thomas.getgood@gmail.com>
2017-10-06 12:55:49 -0400

    Figwheel compatible compiler load-fn.

--- src/canvas_demos/eval.cljs
+++ src/canvas_demos/eval.cljs
@@ -6,8 +6,25 @@
 (def eval-ns (atom 'cljs.user))
 
 (def require-form
-  '(:require [canvas-demos.shapes.affine :refer [translate rotate scale reflect with-origin]]
+  '(:require [canvas-demos.shapes.affine
+              :refer [translate rotate scale reflect with-origin]]
              [canvas-demos.shapes.base :refer [line circle rectangle]]))
+
+(defn- prepend-path [opts]
+  (update opts :path #(str "/js/compiled/out/" %)))
+
+(defn load-fn [opts cb]
+  (if (re-matches #"^goog/.*" (:path opts))
+    (eval-soup/custom-load! (-> opts
+                                (update :path eval-soup/fix-goog-path)
+                                prepend-path)
+                            [".js"]
+                            cb)
+    (eval-soup/custom-load! (prepend-path opts)
+                            (if (:macros opts)
+                              [".clj" ".cljc"]
+                              [".cljs" ".cljc" ".js"])
+                            cb)))
 
 (defn eval
   ([form cb]
@@ -15,28 +32,31 @@
   ([form name cb]
    (let [forms (if (nil? name) [form] [(list 'def name form) name])
          read-cb (fn [results]
-                   (eval-soup/eval-forms (eval-soup/add-timeouts-if-necessary
-                                          forms results)
-                                         cb
-                                         eval-soup/state
-                                         eval-ns
-                                         eval-soup/custom-load!))
+                   (eval-soup/eval-forms
+                    (eval-soup/add-timeouts-if-necessary
+                     forms results)
+                    cb
+                    eval-soup/state
+                    eval-ns
+                    load-fn))
          init-cb (fn [results]
-                   (eval-soup/eval-forms (map eval-soup/wrap-macroexpand forms)
-                                         read-cb
-                                         eval-soup/state
-                                         eval-ns
-                                         eval-soup/custom-load!))]
-     (eval-soup/eval-forms ['(ns cljs.user)
-                            '(def ps-last-time (atom 0))
-                            '(defn ps-reset-timeout! []
-                               (reset! ps-last-time (.getTime (js/Date.))))
-                            '(defn ps-check-for-timeout! []
-                               (when (> (- (.getTime (js/Date.)) @ps-last-time) 5000)
-                                 (throw (js/Error. "Execution timed out."))))
-                            '(set! *print-err-fn* (fn [_]))
-                            (list 'ns @eval-ns require-form)]
-                           init-cb
-                           eval-soup/state
-                           eval-ns
-                           eval-soup/custom-load!))))
+                   (eval-soup/eval-forms
+                    (map eval-soup/wrap-macroexpand forms)
+                    read-cb
+                    eval-soup/state
+                    eval-ns
+                    load-fn))]
+     (eval-soup/eval-forms
+      ['(ns cljs.user)
+       '(def ps-last-time (atom 0))
+       '(defn ps-reset-timeout! []
+          (reset! ps-last-time (.getTime (js/Date.))))
+       '(defn ps-check-for-timeout! []
+          (when (> (- (.getTime (js/Date.)) @ps-last-time) 5000)
+            (throw (js/Error. "Execution timed out."))))
+       '(set! *print-err-fn* (fn [_]))
+       (list 'ns @eval-ns require-form)]
+      init-cb
+      eval-soup/state
+      eval-ns
+      load-fn))))
